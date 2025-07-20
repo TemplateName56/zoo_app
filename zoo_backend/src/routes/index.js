558caf218decbb979dcaabcd69df22a3fef81182
TemplateName56 <deabrua.sendoa@gmail.com>
2025-07-20 14:03:37 +0300

    Delete unused code.

--- zoo_backend/src/routes/index.js
+++ zoo_backend/src/routes/index.js
@@ -4,7 +4,6 @@ const router = express.Router();
 const authRoutes = require("./users/auth");
 const animalRoutes = require("./animals/animal");
 const chatRoutes = require("./chats/chat");
-const uploadRoutes = require("./uploads/upload");
 const bookmarkRoutes = require("./bookmarks/bookmark");
 const adminRoutes = require("./users/admin");
 const userRoutes = require("./users/user");
